1e2aa9bea3b4549d32348ebd118da44cc949915c
akhilperam <akhiakhil96@gmail.com>
2023-02-16 23:41:07 +0530

    First Commit

---
+++ Example/src/in/ineuron/main/StudentOne.java
@@ -0,0 +1,43 @@
+package in.ineuron.main;
+
+class Student
+{
+	private int age;
+	private String city;
+	private String Name;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+}
+
+public class StudentOne {
+
+	public static void main(String[] args) {
+		Student st=new Student();
+		st.getAge();
+		st.getCity();
+		st.getName();
+		st.setAge(25);
+		st.setName("Akhil");
+	    st.setCity("Hyderabad");
+	    System.out.println(st.getAge());
+	    System.out.println(st.getName());
+	    System.out.println(st.getCity());
+	}
+
+}
